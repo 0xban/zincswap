831c32d79dee7dad2989afde6cf86698cc37d710
ban <lanai.development@gmail.com>
2022-05-02 12:16:55 -0400

    Update SwapPage.jsx

--- client/src/pages/SwapPage.jsx
+++ client/src/pages/SwapPage.jsx
@@ -1,11 +1,7 @@
 const SwapPage = () => {
   return (
     <div className="SwapPage">
-          <div className="gradient-bg">
-            <h1 className="text-white">
-                sup
-            </h1>
-          </div>
+        
     </div>
   )
 }
